e8349d94815954d9138be236618ee2c7005d81b1
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-11-23 11:37:23 +0000

    innit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  return (
+    <div>
+      <h1>Eva Rut</h1>
+    </div>
+  );
+};
+
+
+export default App;
+
+
+// import Header from './components/Header';
+// import AboutMe from './components/AboutMe';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <AboutMe />
+//       <Projects />
+//       <Contact />
+//     </div>
+//   );
+// };
+
+// export default App;
