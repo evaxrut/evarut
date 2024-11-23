210341c7c4c311ff25254dcb81fc78af0f881847
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-11-23 11:44:20 +0000

    innit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -23,6 +23,8 @@ import './App.css';
 //   );
 // }
 
+// export default App;
+
 const App = () => {
   return (
     <div>
@@ -31,24 +33,4 @@ const App = () => {
   );
 };
 
-
 export default App;
-
-
-// import Header from './components/Header';
-// import AboutMe from './components/AboutMe';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
-
-// const App = () => {
-//   return (
-//     <div>
-//       <Header />
-//       <AboutMe />
-//       <Projects />
-//       <Contact />
-//     </div>
-//   );
-// };
-
-// export default App;
